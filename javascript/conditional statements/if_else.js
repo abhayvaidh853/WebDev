1e82836bb262aff75879ea40243fe6611ae8c02a
abhayvaidh853 <abhayvaidh853@gmail.com>
2026-06-16 18:10:44 +0530

    JavaScript

--- javascript/conditional statements/if_else.js
+++ javascript/conditional statements/if_else.js
@@ -16,18 +16,20 @@
 // const prompt = require("prompt-sync")();
 // let marks = prompt("enter ur marks")
 
-let marks = 75
+// let marks = 75
 
 
-if (marks >=90) {
-    console.log("Grade A")
-}
-else if (marks >= 70) {
-    console.log("Grade B")
-}
-else if (marks >= 50){
-    console.log("Grade C")
-}
- else {
-    console.log("Fail")
-}
+// if (marks >=90) {
+//     console.log("Grade A")
+// }
+// else if (marks >= 70) {
+//     console.log("Grade B")
+// }
+// else if (marks >= 50){
+//     console.log("Grade C")
+// }
+//  else {
+//     console.log("Fail")
+// }
+
+
