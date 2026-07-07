6d7903df1c9cb405ab7e754da857d1011abdcce8
abhayvaidh853 <abhayvaidh853@gmail.com>
2026-07-07 18:01:56 +0530

    condition

---
+++ javascript/loops/dowhile.js
@@ -0,0 +1,80 @@
+// let i = 1
+
+// let cd = true
+// while (cd) {
+//     console.log(i)
+//     i++
+
+//     if ( i == 20){
+//         cd = false
+//     }
+// }
+
+
+//Print odd numbers
+// i = 1
+// let count = 0
+
+// do{
+//     console.log(i)
+//     count++
+//     i = i + 2
+// }
+
+// while(i <= 20)
+// console.log("Count of even number is :",count)
+
+
+//Print odd numbers
+
+// let i = 1
+// let count = 0
+
+// do{
+//     console.log(i)
+//     count++
+//     i = i + 2
+// }
+
+// while(i <= 20)
+// console.log("Count of odd number is :",count)
+
+
+// Print Even numbers
+
+// let i = 1
+// let count = 0
+
+// do {
+//     if (i % 2 == 0){
+//         console.log("This id even",i)
+//     }
+//     else{
+//         console.log("This is odd ",i)
+//     }
+//     i++
+// }
+// while(i <= 20 )
+//     console.log("Count of even numbers is :",count)
+
+
+//Nested Loops
+
+// for (initialization; condition; inc/dec){
+    
+//     for (initialization; condition; inc/dec){
+
+//         //code
+
+//     }
+// }
+
+//Example 1 :
+
+// for (i = 1; i<= 3; i++){
+//     console.log("Outer Loop",i)
+//     for(let j = 1; j <= 3; j++){
+//         console.log("Inner Lopp",j)
+//     }
+// }
+
--- javascript/loops/while.js
+++ javascript/loops/while.js
@@ -81,12 +81,101 @@
 
 // Palindrome Number
 
-let num = 121
-let original = num
-let reverse = 0
+// let num = 121
+// let original = num
+// let reverse = 0
 
-while(num > 0){
-    let digit = num % 10
-    reverse = reverse * 10 + digit
-    num = Math.floor(num / 10)
+// while(num > 0){
+//     let digit = num % 10
+//     reverse = reverse * 10 + digit
+//     num = Math.floor(num / 10)
+// }
+
+// if (original == reverse){
+//     console.log("Palindrome")
+// }else{
+//     console.log("Not Palindrome")
+// }
+
+
+//Login password retry System
+
+// const prompt = require("prompt-sync")();
+
+// let password = ""
+// let attempts = 3
+
+// while(attempts > 0 ){
+//     password = prompt("Enter your password :")
+
+//     if (password == "1234"){
+//         console.log("Login Successful")
+//         break;
+//     }else{
+//         attempts--
+//         console.log("Wrong password")
+//         console.log("Remaining attempts :",attempts)
+//     }
+// }
+
+// if(attempts == 0){
+//     console.log("Account Locked")
+// }
+
+
+//Question = Number Guessing Game
+
+// const prompt = require("prompt-sync")();
+
+// let randomeNum = Math.floor(Math.random () * 10)+1
+
+// let guess = 0;
+// let attempts = 0;
+// while (guess !== randomeNum){
+//     guess = Number(prompt("Guess a number :"))
+//     attempts++
+
+
+//     if (guess > randomeNum){
+//         console.log("Too High")
+//     }
+//     else if (guess < randomeNum){
+//         console.log("Too Low")
+//     }
+//     else{
+//         console.log("Congratulations ! You Have Guesse it Correctly")
+//         console.log("attempts ",attempts)
+//     }
+// }
+
+
+// Library Book Search System
+
+const prompt = require("prompt-sync")();
+
+let search = ""
+
+while (search !== "exit"){
+    search = prompt("/nEnter Book Name ( or type ' exit)");
+
+    if (search.toLocaleLowerCase() == "javascript"){
+        confirm.log("javascript book found")
+    }
+    else if (search.toLocaleLowerCase() == "python"){
+        confirm.log("python book found")
+    }
+    else if (search.toLocaleLowerCase() == "java"){
+        confirm.log("java book found")
+    }
+    else if (search.toLocaleLowerCase() == "C++"){
+        confirm.log("C++ book found")
+    }
+    else if (search.toLocaleLowerCase() == "exit"){
+        confirm.log("Thank You ! visit Again")
+    }
+    else{
+        console.log("Book Nor Available")
+    }
+    
 }
+
