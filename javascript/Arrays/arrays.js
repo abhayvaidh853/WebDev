6d0816777c57f03d61cd28f76a2c7c7358c39845
abhayvaidh853 <abhayvaidh853@gmail.com>
2026-07-14 18:22:51 +0530

    Slice

---
+++ javascript/Arrays/arrays.js
@@ -0,0 +1,126 @@
+// Arrays
+//let marks = [76,88,99,66]
+
+// let numbers = [10,20,30,40,50,60,70,80]
+
+// //arrays the elements
+// console.log(numbers[5])
+
+// //update the value
+// numbers[2] = 100
+
+// console.log(numbers)
+
+// let arr = [10,20,30,40,50]
+
+// console.log(arr[arr.length-1])
+
+
+// let data = ["Abhay",20,true,99.5,null,undefined]
+// console.log(data)
+
+// let arr = [
+//     [1,2],
+//     [1,4,5],
+//     [1,6]
+// ]
+
+// console.log(arr)
+
+// Basic Array Methods
+
+//push - last me add
+
+// let fruits = ["Apple","Mango","Kiwi"]
+// fruits.push("Banana")
+// console.log(fruits)
+
+//pop()- Last se remove 
+
+// let fruits = ["Apple","Mango","Kiwi"]
+// fruits.pop()
+// console.log(fruits)
+
+//shift() - starting se remove
+
+// let fruits = ["Apple","Mango","Kiwi"]
+
+// fruits.shift()
+
+// console.log(fruits)
+
+// console.log(fruits.length)
+
+
+// //unshift() - starting me add
+
+// let fruits = ["Apple","Mango","Kiwi"]
+
+// fruits.unshift(Samsung)
+
+// console.log(fruits)
+
+
+//includes()
+
+// let fruits = ["Apple","Mango","Kiwi","banana","cherry","grapes","papaya"]
+
+// console.log(fruits.includes("Aam"))
+
+
+//Lastindex01
+
+// let fruits = ['Apple',"banana","Mango","Kiwi","Cherry","Grapes","Apple","Papaya"]
+
+// console.log(fruits.indexOf("Mango"))
+// console.log(fruits.lastIndexOf(""))
+
+
+//at()
+
+// let numbers = [10,20,30,40,50]
+
+// console.log(numbers.at(-1))//negetive indexing ke liye
+// console.log(numbers.at(-2))
+// console.log(numbers.at(-3))
+
+
+//Fruits Joint
+
+// let fruits = ['Apple',"banana","Mango","Kiwi","Cherry","Grapes","Apple","Papaya"]
+
+// console.log(fruits.join("➜ "))
+
+
+// 11. Revrse
+
+// let numbers = [10,20,30,40,50]
+
+// numbers.reverse()
+// console.log(numbers)
+
+
+// 12. Sort
+
+// let numbers = [5,7,4,9,6,1,3,2]
+// let fruits = ['Apple',"banana","Mango","Kiwi","Cherry","Grapes","Apple","Papaya"]
+
+// numbers.sort((a,b)=> a+b)
+
+// console.log(fruits)
+
+
+// 13.Slice()-array se kuch elements ki copy nikalta hai, lekin original array ko change nahi karta.
+
+// let numbers = [10,20,30,40,50]
+
+// console.log(numbers.slice(0,5))
+
+
+//14. splice()-ka main kaam original array ko modify (change) karna hai
+
+// let numbers = [10,20,30,40,50]
+
+// numbers.splice(0,2)
+
+// console.log(numbers)
