ce560dcbe1999abfbdfe9dacd91d2701db085191
abhayvaidh853 <abhayvaidh853@gmail.com>
2026-07-16 19:45:54 +0530

    function

---
+++ javascript/Functions/function.js
@@ -0,0 +1,13 @@
+// Function 
+
+// function greet(name){
+//     console.log("Welcome to Techno Skill",name)
+// }
+
+// greet("Abhay")
+
+//Welcome Student , Abhay for Enroll
+//Welcome Student , rishi Enroll
+//Welcome Student , nishant for Enroll
+//Welcome Student , rohit for Enroll
+//Welcome Student , shubham for Enroll
