4d643ecb608e04a4d645d2cf8af25b41a5291b0e
abhayvaidh853 <abhayvaidh853@gmail.com>
2026-07-17 22:06:38 +0530

    functions

--- javascript/Functions/function.js
+++ javascript/Functions/function.js
@@ -11,3 +11,98 @@
 //Welcome Student , nishant for Enroll
 //Welcome Student , rohit for Enroll
 //Welcome Student , shubham for Enroll
+
+
+//Function to print my details
+
+// function myDetails(){
+//     console.log("Name : Abhay")
+//     console.log("City : Balaghat")
+//     console.log("Course : JavaScript")
+    
+// }
+
+// //Function ko call kr kr rhe h 
+
+// myDetails()
+
+// function college(){
+//     console.log("Collge_Name : MCU")
+//     console.log("Review : Bhut hi bakwas clg hai")
+//     console.log("Advantage : Wi-fi h bass")
+// }
+
+// college()
+
+// function teacher(){
+//     console.log("Teacher_Name : Abhay")
+//     console.log("Skill : Web Developer")
+//     console.log("Address : Balaghat")
+// }
+
+// teacher()
+
+
+//Function with parameter
+
+// function greet(name,age,msg = "hello"){
+//     console.log(msg,name,"my age is :",age )
+// }
+
+// greet("hii","Abhay",19)
+
+
+//Function with return value
+
+// function add(a,b){
+//     return a + b
+// }
+// let result = add(7,4)
+// let result1 = add(999,1)
+
+// console.log(result,result1)
+
+
+//Function Experation 
+
+// const greet = function (){
+//     console.log("Heloo Student")
+// }
+
+// greet()
+
+//Anonymous Function
+//jiska koi name nhi hota
+
+// setTimeout(function(){
+//     console.log("Hello")
+// },5000)
+// console.log("hiii")
+
+
+//Arrow Function ES 6 2015 me
+//Morden JS Functio
+
+// const greet = () => {
+//     console.log("Heloo Students")
+
+// }
+// greet()
+
+
+// const greet = (name) => {
+//     console.log("Hello World")
+// }
+
+// greet("Abhay")
+
+//normal function
+// function square(num){
+//     return num * num
+// }
+
+
+//arrow function
+// const square = num => num * num
+
+// console.log(square(2))
