8dac1209dd4649ac3137bf7bccb561facfd68bee
abhayvaidh853 <abhayvaidh853@gmail.com>
2026-07-18 18:20:16 +0530

    type_of_functions

--- javascript/Functions/function.js
+++ javascript/Functions/function.js
@@ -108,3 +108,114 @@
 // console.log(square(2))
 
 
+//CallBack Function
+
+// function greet(name){
+//     console.log("Hello",name)
+// }
+
+// function process(callback){
+//     callback("Rahul")
+// }
+
+// process(greet)
+
+
+//Example of Callback Function
+
+// function receipt(){
+//     console.log("Receipt Printed")
+
+// }
+// function withdraw(callback){
+//     console.log("Cash Withdraw")
+//     callback()
+// }
+
+// function deposite(callback){
+//     console.log("Cash Deposite")
+//     callback()
+// }
+
+// deposite(receipt)
+
+//Example 2. Car Service
+
+// function CustomerCall(){
+//     console.log("Calling Customer")
+// }
+
+// function repair(callback){
+//     console.log("Repair Completed")
+// }   callback()
+
+// repair(CustomerCall)
+
+//Example 3. Function Delivery
+
+// function delivery(){
+//     console.log("Pizza Delivered")
+// }
+
+// function makePizza(){
+//     console.log("Makeing Pizz....")
+//     console.log("PIzza Ready")
+//     useCallback()
+// }
+
+// makePizza(delivery)
+
+
+//Real Life Example of washing machine
+//callbacl ek aisa function h jo pass kiya jata h dusre function ko as a argument !
+
+
+// function beep(){
+//     console.log("beep beep beep")
+// }
+
+// function washing(callback){
+//     console.log("Kapde Dhote Jao Dhote Jao Dhote jao Dho....")
+//     console.log("Kapde Dho kr Ho gye hai! ")
+
+//     callback()
+// }
+
+
+// washing(beep)
+
+
+
+//Recuesive Function - Jab function khud ko hi call krta h !
+//Factorial function ko print krne ke kam me aata h
+
+
+// function countdown(n){
+//     if (n==0){
+//         console.log("Done")
+//         return
+//     }
+
+//     console.log(n)
+//     countdown(n-1)
+// }
+
+// countdown(5)
+
+
+//Default Default Parameter
+
+// function greet(name = "Guest"){
+
+//     console.log("Hello",name)
+// }
+
+// greet("abhay")
+
+
+//Function with rest parameter 3 dot dene pr
+
+// function print(...numbers){
+//     console.log(numbers)
+// }
+// print(10,20,40,60,70,80)
