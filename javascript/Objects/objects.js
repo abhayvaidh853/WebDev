b06f797a960ef00533f1e7e02520ffb839ae9c38
abhayvaidh853 <abhayvaidh853@gmail.com>
2026-07-25 18:28:27 +0530

    objects

---
+++ javascript/Objects/objects.js
@@ -0,0 +1,84 @@
+
+// let student1 = {
+//     name: "Abhay",
+//     greet(){
+//         console.log(this.age)
+//     }
+    
+// }
+
+// let student2 = {
+//     name: "Rishee",
+    
+// }
+
+// let student3 = {
+//     name: "Nishant",
+    
+// }
+
+// console.log(this.name)
+
+
+//Object Destrutring
+
+// let student = {
+//     name: "Abhay",
+//     age: 20,
+//     city: "Delhi"
+// }
+
+// let name = student.name
+// let age = student.age
+// let city = student.city
+
+// console.log(name)
+// console.log(age)
+// console.log(city)
+
+//with Destructring
+
+// let student = {
+//     name: "Abhay",
+//     age: 20,
+//     city: "Delhi"
+// }
+
+// let {name,age,city }= student
+
+// console.log(name)
+// console.log(age)
+// console.log(city)
+
+
+//Sqread Operator
+//(. . .)
+
+// let student = {
+//     name: "Abhay",
+//     age: 20 ,
+//     roll:6644,
+//     course:"Python"
+// }
+
+// let details = {
+//     ...student,
+//     city: "Delhi"
+// }
+
+// console.log(details)
+
+
+// Rest Operator (Objects)
+
+// let student ={
+//     name: "Abhay",
+//     age: 20,
+//     city: "Delhi",
+//     course: "JS"
+// }
+
+// let {...rest}= student
+
+// console.log(rest)
+// // console.log(name)
