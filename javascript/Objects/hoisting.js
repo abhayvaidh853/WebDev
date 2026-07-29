90b0238220674c20bf586a5e3a7cf57196cf1345
abhayvaidh853 <abhayvaidh853@gmail.com>
2026-07-29 18:35:36 +0530

    hoisting

---
+++ javascript/Objects/hoisting.js
@@ -0,0 +1,19 @@
+
+
+// console.log(name)
+// var name ="Abhay"
+// // let name = "Abhay"
+
+// // tamporarl death zone
+
+// console.log(total)
+// let total = 500
+
+// Hoisting Javascript ka ek behaviour h,
+// jisme code execute hone se phle veriables or funtions ki declaretiom
+// ko procecc kiya jata h.
+
+
+// TdZ : vo time hota h jisme let or cosnt se bane veriable create to ho gye h pr 
+// unko jis line me create kiye gye h 
+// us line me phle use nhi kr skte h  or access kiye to reference error aayega.
